7fa0ab2d8cd9a5db7f680c467bbba811fd6a31c4
mdbellalxpro <mdbellalprocoder@gmail.com>
2024-12-06 15:43:43 +0600

    added new data

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
+// Access the environment variable
+const uri = process.env.MONGO_URI;
+
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/searchDB', {
-    
+mongoose.connect(uri)
+.then(() => {
+    console.log("Connected to MongoDB Atlas!");
 })
-.then(() => console.log('MongoDB connected'))
-.catch((error) => console.log('MongoDB connection error:', error));
-  
+.catch(err => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
+// Example schema and model
+const exampleSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
 
 // Use routes
 app.use('/api/items', itemRoutes);
 
 
-
 // server runnig put
 const port = process.env.PORT || 8080;
-const SECRET_KEY = process.env.SECRET_KEY;
+
 app.listen(port, () => console.log(`Server run http://localhost:${port}`));
 
